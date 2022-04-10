1476fa78daaeedfa7296f8b49ab9b5b24470d6c7
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-04-10 11:33:15 -0500

    making the bases

--- Ejercicio4/Ejercicio4/Program.cs
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -6,7 +6,21 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cifrar();
+        }
+
+        static void Cifrar()
+        {
+            Console.WriteLine("Ingrese el valor de posiciones a desplazar");
+            int desplazamineto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cadena a cifrar");
+            string cadena = Console.ReadLine();
+            byte[] asciiBytes = System.Text.Encoding.ASCII.GetBytes(cadena);
+            foreach (var item in asciiBytes)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
