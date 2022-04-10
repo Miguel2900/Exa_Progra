26eae09cca7d9ab5c9c149c8e598724094d724c5
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-04-10 15:13:59 -0500

    Ejercicio 5 finished

--- Ejercicio5/Ejercicio5/Program.cs
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -13,9 +13,21 @@ namespace Ejercicio5
         {
             Console.WriteLine("Ingrese la cadena que desee desencriptar");
             string cadenaEncriptada = Console.ReadLine();//cadena encriptada
-            for (int i = 0; i < 94; i++)
+            Console.Clear();
+            for (int i = 0; i < 94; i++)//Se intentara 94 intentos de desencriptacion, ya que eso es la cantidad de caracteres en el limite
             {
-
+                string cadenaDesencriptada = "";//cadena en donde se guardara la posible desencriptacion
+                foreach (var item in cadenaEncriptada)
+                {
+                    int valor = item - (i + 1);//valor de desplazamiento que quiza se realizo
+                    if (valor < 32)//se valida si el valor es menor a 32
+                        valor += 95;// en caso de ser menor a 32, se debe llevar al final del limite
+                                    //por ejemplo si se tiene un caracter " ", es decir char(32)
+                                    //y se prueba descifrar con un desplazamiento de 1, quedaria char(31)
+                                    //por ende, se debe de llevar de nuevo al final del limite, el caracter "~", char (126)
+                    cadenaDesencriptada += (char)valor;
+                }
+                Console.WriteLine("Opcion con {0} desplazamientos: {1}", i + 1, cadenaDesencriptada);//se despliega la posible solucion de desencriptado
             }
         }
     }
