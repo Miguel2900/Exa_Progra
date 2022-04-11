5505c30c75998af5afd0d6a08ae4ff1389dccc39
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-04-10 19:01:42 -0500

    Ejercicio1 finished

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -16,46 +16,27 @@ namespace Ejercicio1
             Console.WriteLine("Ingrese el largo del laberinto");
             int largo = int.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese la proporcion de paredes del laberinto");
-            decimal proporcion = (decimal.Parse(Console.ReadLine()) / 100) * (ancho * largo);
-            int vacias = (ancho * largo) - (int)proporcion;
-            int count = 0;
-            //for (int i = 0; i < ancho; i++)
-            //{
-            //    for (int j = 0; j < largo; j++)
-            //    {
-            //        char valor = ' ';
-            //        if (i == 0 || i == ancho - 1)
-            //        {
-            //            valor = '█';
-            //        }
-            //        if (i > 0 && i < ancho - 1)
-            //        {
-            //            Random random = new Random();
-            //            if (random.Next(10) == 2 && count < proporcion)
-            //            {
-            //                valor = '█';
-            //                count++;
-            //            }
-            //        }
-            //        Console.Write(valor);
-            //    }
-            //    Console.Write("\n");
-            //}
+            decimal proporcion = (decimal.Parse(Console.ReadLine()) / 100) * (ancho * largo);//Operacion para obtener cuantas celdas deberian ser paredes
+            int count = 1;//varaible que servira para crear el numero correcto de paredes
             for (int i = 0; i < ancho * largo; i++)
             {
-                char valor = '█';
-                if (i == largo || i == (ancho * largo) - 1 - largo || i == largo + 1 || i == (ancho * largo) - 2 - largo)
+                Random random = new Random();
+                char valor = ' ';
+                if (i < largo || i >= (ancho * largo) - largo || i % largo == 0 || i % largo == largo -1)//valida si es alguno de los extremos del laberinto
                 {
-                    valor = ' ';
+                    if(i != largo && i != (ancho * largo) - 1 - largo)//representa a la entrada y salida, haciendo que nunca esten cerradas
+                        valor = '█';
                 }
-                if (i %largo != 0 || i %largo != largo - 1)
+                else if (random.Next(3) == 1 && count < proporcion)
                 {
-
+                    valor = '█';
+                    count++;
                 }
                 Console.Write(valor);
                 if ((i)%largo ==largo -1)
                     Console.Write("\n");
             }
+            Console.WriteLine("Se hciieron {0} a comparacion de {1}",count,proporcion);
         }
     }
 }
