31e611615eb951755a5dd7880268ba18c8df5bda
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-04-10 19:02:13 -0500

    Comments

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -18,25 +18,25 @@ namespace Ejercicio1
             Console.WriteLine("Ingrese la proporcion de paredes del laberinto");
             decimal proporcion = (decimal.Parse(Console.ReadLine()) / 100) * (ancho * largo);//Operacion para obtener cuantas celdas deberian ser paredes
             int count = 1;//varaible que servira para crear el numero correcto de paredes
+            Console.Clear();
             for (int i = 0; i < ancho * largo; i++)
             {
                 Random random = new Random();
-                char valor = ' ';
+                char valor = ' ';//Celda vacia
                 if (i < largo || i >= (ancho * largo) - largo || i % largo == 0 || i % largo == largo -1)//valida si es alguno de los extremos del laberinto
                 {
                     if(i != largo && i != (ancho * largo) - 1 - largo)//representa a la entrada y salida, haciendo que nunca esten cerradas
-                        valor = '█';
+                        valor = '█';//pared
                 }
-                else if (random.Next(3) == 1 && count < proporcion)
+                else if (random.Next(3) == 1 && count < proporcion)//En caso de no ser un borde, se usa un random para saber si puede o no ser una pared
                 {
-                    valor = '█';
-                    count++;
+                    valor = '█';//pared
+                    count++;//se va incrementando el contador para poner solo las paredes necesarias
                 }
-                Console.Write(valor);
-                if ((i)%largo ==largo -1)
+                Console.Write(valor);//Se imprime el valor ya sea el caso
+                if ((i)%largo ==largo -1)//salto de linea para que pueda ser cuadrado
                     Console.Write("\n");
             }
-            Console.WriteLine("Se hciieron {0} a comparacion de {1}",count,proporcion);
         }
     }
 }
